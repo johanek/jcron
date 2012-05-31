2b8452d0c1e9cc97f0ad6ba28f0ce0f083da52a6
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2012-05-31 22:06:06 +0100

    removed find feature

--- bin/jcron.rb
+++ bin/jcron.rb
@@ -10,14 +10,6 @@ def die(msg)
   exit 1
 end
 
-def parser(hash)
-  hash['rows'].each do |row|
-    row['value'].each_pair do |k,v|
-      puts "#{k} - #{v}"
-    end
-  end
-end
-
 class CLI < Thor
 
   desc "ex [cmd]", "run job"
@@ -29,14 +21,6 @@ class CLI < Thor
     job.run(cmd)
   end
   
-  desc "find [cmd]", "find cmd history"
-  def find(*cmd)
-    die("Must provide command to find") unless cmd.any?
-    cmd = cmd.join(' ')
-    server = 'couchdb'
-    job = Jcron::Findjob.new(server)
-    parser(job.find(cmd))
-  end
 end
 
 CLI.start
