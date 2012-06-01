8d8ce43afdf73175a0d41da760b5e122a9ed39f4
Johan van den Dorpe <jvd@dneg.com>
2012-06-01 11:23:10 +0100

    add eventasaurus publishing

--- bin/jcron.rb
+++ bin/jcron.rb
@@ -3,7 +3,9 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'rubygems'
 require 'thor'
+require 'socket'
 require 'jcron'
+require 'eventasaurus'
 
 def die(msg)
   puts msg
@@ -18,9 +20,21 @@ class CLI < Thor
     cmd = ARGV.join(' ')
     die("Must provide command to ex") unless ARGV.any?
 
+    # Run Job
     server = 'couchdb'
     job = Jcron::Runjob.new(server)
     job.run(cmd)
+
+    # Publish
+    stomp = Eventasaurus::Producer.new('event')
+    stomp.topic = 'eventasaurus'
+    msg = {
+      'ident' => 'cron',
+      'timestamp' => Time.now.utc.iso8601,
+      'message' => "Cronjob #{cmd} finished on ##{Socket.gethostname} http://couchdb:5984/_utils/database.html?cron"
+    }
+    stomp.pub(msg.to_json)
+    stomp.close
   end
   
 end
