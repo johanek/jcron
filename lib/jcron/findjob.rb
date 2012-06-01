877bd8fa4783bc6f35258222e208d7de12859178
Johan van den Dorpe <jvd@dneg.com>
2012-06-01 17:48:31 +0100

    add findjob

---
+++ lib/jcron/findjob.rb
@@ -0,0 +1,16 @@
+module Jcron
+  class Findjob
+
+    def initialize(server, port='5984')
+      @baseuri = "/cron/"
+      @couchdb = Jcron::Couch.new(server, port)
+    end
+    
+    def find(cmd)
+      uri = @baseuri + "_design/cronview/_view/by_cmd?key=\"#{cmd}\""
+      result = @couchdb.get(uri)
+      JSON.parse(result.body)
+    end
+
+  end
+end
