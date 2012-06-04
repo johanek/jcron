a66ec4fea89a29e4ad0e9fd0792a45bcba1ec937
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2012-06-04 20:52:31 +0100

    New DB/View creation script

---
+++ install/setup-db.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'jcron'
+require 'json'
+
+dbserver = 'couchdb'
+dbport = 5984
+db = 'jcron'
+dburi = "/#{db}/"
+viewuri = "#{dburi}/_design/cronview"
+
+# Create DB
+couch = Jcron::Couch.new(dbserver, dbport)
+# put requires json content, but db server ignores this content anyway
+nothing = {}
+couch.put(dburi, nothing.to_json)
+
+# Create View
+view = {
+  "_id" => "_design/cronview",
+  "language" => "javascript",
+  "views" =>
+	{
+	   "by_host" => {
+	       "map" => "function(doc) { if (doc.host)  emit(doc.host, doc) }"
+	   },
+	   "by_runtime" => {
+	       "map" => "function(doc) { if (doc.runtime)  emit(doc.runtime, doc) }"
+	   },
+	   "by_starttime" => {
+	       "map" => "function(doc) { if (doc.starttime)  emit(doc.starttime, doc) }"
+	   },
+	   "by_endtime" => {
+	       "map" => "function(doc) { if (doc.endtime)  emit(doc.endtime, doc) }"
+	   },
+	   "by_cmd" => {
+	       "map" => "function(doc) { if (doc.cmd)  emit(doc.cmd, doc) }"
+	   },
+	   "by_exitcode" => {
+	       "map" => "function(doc) { if (doc.exitcode)  emit(doc.exitcode, doc) }"
+	   },
+	   "by_cmd_keyword" => {
+         "map" => "function(doc) { var stringarray = doc.cmd.split(/[^A-Z0-9\-_]+/i); for(var idx in stringarray) emit(stringarray[idx],doc); }"
+     }
+	}
+}
+couch.put(viewuri, view.to_json)
