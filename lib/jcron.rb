9d24252aee9a62fc92355e27636b3a58ac4beb83
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2012-06-05 21:10:54 +0100

    added config file support

--- lib/jcron.rb
+++ lib/jcron.rb
@@ -6,13 +6,18 @@ require 'json'
 require 'socket'
 require 'uri'
 require 'time'
+require 'yaml'
+require 'cron-spec'
+require 'eventasaurus'
+require 'socket'
 require 'jcron/runjob'
 require 'jcron/couch'
 require 'jcron/findjob'
+require 'jcron/schedule'
 
 module Jcron
   class << self
-    
+        
     def short_isotime(time)
       time.utc.strftime("%Y-%m-%dT%H:%MZ")
     end
